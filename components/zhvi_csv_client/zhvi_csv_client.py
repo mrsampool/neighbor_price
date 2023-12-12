4376cf09512f5b674c34cad9456aa9c8fdbb3c99
sampool <sambpool@gmail.com>
2023-12-12 08:29:34 -0700

    add metro & state csv paths

--- components/zhvi_csv_client/zhvi_csv_client.py
+++ components/zhvi_csv_client/zhvi_csv_client.py
@@ -32,10 +32,14 @@ class ZhviCsvClient:
     def __init__(
             self,
             zhvi_csv_url: str,
-            zvhi_neighborhood_csv_path: str
+            zvhi_neighborhood_csv_path: str,
+            zvhi_metro_csv_path: str,
+            zvhi_state_csv_path: str
     ):
         self.zhvi_csv_url = zhvi_csv_url
         self.zhvi_neighborhood_csv_path = zvhi_neighborhood_csv_path
+        self.zhvi_metro_csv_path = zvhi_metro_csv_path
+        self.zhvi_state_csv_path = zvhi_state_csv_path
 
     def _get_zhvi_df_from_path(self, path):
         response = requests.get(self.zhvi_csv_url + path)
@@ -45,3 +49,9 @@ class ZhviCsvClient:
 
     def get_zhvi_neighborhoods_df(self):
         return self._get_zhvi_df_from_path(self.zhvi_neighborhood_csv_path)
+
+    def get_zhvi_metros_df(self):
+        return self._get_zhvi_df_from_path(self.zhvi_metro_csv_path)
+
+    def get_zhvi_states_df(self):
+        return self._get_zhvi_df_from_path(self.zhvi_state_csv_path)
--- data_collector/__main__.py
+++ data_collector/__main__.py
@@ -11,16 +11,6 @@ app = Flask(__name__)
 
 class Config:
     def __init__(self):
-        self.zhvi_csv_url = os.getenv("ZHVI_CSV_URL")
-        logging.info(f"using ZHVI_CSV_URL: {self.zhvi_csv_url}")
-        if self.zhvi_csv_url is None:
-            logging.fatal("Missing required ENV: $ZHVI_CSV_URL")
-
-        self.zhvi_neighborhood_csv_path = os.getenv("ZHVI_NEIGHBORHOOD_CSV_PATH")
-        logging.info(f"using ZHVI_NEIGHBORHOOD_CSV_PATH: {self.zhvi_neighborhood_csv_path}")
-        if self.zhvi_neighborhood_csv_path is None:
-            logging.fatal("Missing required ENV: $ZHVI_NEIGHBORHOOD_CSV_PATH")
-
         self.zhvi_db_uri = os.getenv("ZHVI_DB_URI")
         logging.info(f"using ZHVI_DB_URI: {self.zhvi_db_uri}")
         if self.zhvi_db_uri is None:
@@ -31,13 +21,35 @@ class Config:
         if self.zhvi_db_name is None:
             logging.fatal("Missing required ENV: $ZHVI_DB_NAME")
 
+        self.zhvi_csv_url = os.getenv("ZHVI_CSV_URL")
+        logging.info(f"using ZHVI_CSV_URL: {self.zhvi_csv_url}")
+        if self.zhvi_csv_url is None:
+            logging.fatal("Missing required ENV: $ZHVI_CSV_URL")
+
+        self.zhvi_neighborhood_csv_path = os.getenv("ZHVI_NEIGHBORHOOD_CSV_PATH")
+        logging.info(f"using ZHVI_NEIGHBORHOOD_CSV_PATH: {self.zhvi_neighborhood_csv_path}")
+        if self.zhvi_neighborhood_csv_path is None:
+            logging.fatal("Missing required ENV: $ZHVI_NEIGHBORHOOD_CSV_PATH")
+
+        self.zhvi_metro_csv_path = os.getenv("ZHVI_METRO_CSV_PATH")
+        logging.info(f"using ZHVI_METRO_CSV_PATH: {self.zhvi_metro_csv_path}")
+        if self.zhvi_metro_csv_path is None:
+            logging.fatal("Missing required ENV: $ZHVI_METRO_CSV_PATH")
+
+        self.zhvi_state_csv_path = os.getenv("ZHVI_STATE_CSV_PATH")
+        logging.info(f"using ZHVI_STATE_CSV_PATH: {self.zhvi_state_csv_path}")
+        if self.zhvi_state_csv_path is None:
+            logging.fatal("Missing required ENV: $ZHVI_STATE_CSV_PATH")
+
 
 def handler():
     c = Config()
 
     csv_client = ZhviCsvClient(
         zhvi_csv_url=c.zhvi_csv_url,
-        zvhi_neighborhood_csv_path=c.zhvi_neighborhood_csv_path
+        zvhi_neighborhood_csv_path=c.zhvi_neighborhood_csv_path,
+        zvhi_metro_csv_path=c.zhvi_metro_csv_path,
+        zvhi_state_csv_path=c.zhvi_state_csv_path,
     )
 
     zn_data_gateway = ZhviNeighborhoodDataGateway(db_uri=c.zhvi_db_uri, db_name=c.zhvi_db_name)
