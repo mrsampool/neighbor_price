31421f3057d09d7ee2566b8a7114d96eaa916b9c
sampool <sambpool@gmail.com>
2023-12-12 06:45:18 -0700

    add zhvi_history to create_neighborhood_record

--- src/components/zhvi_neighborhoods/zhvi_neighborhood_data_gateway.py
+++ src/components/zhvi_neighborhoods/zhvi_neighborhood_data_gateway.py
@@ -2,8 +2,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 import pymongo
+from typing import List
 
-from src.components.zhvi_neighborhoods.zhvi_neighborhood_record import ZhviNeighborhoodRecord, db
+from src.components.zhvi_neighborhoods.zhvi_neighborhood_record import ZhviNeighborhoodRecord
+from src.components.zhvi_history.zhvi_history_item import ZhviHistoryItem
 
 DB_COLLECTION_NEIGHBORHOODS = "neighborhoods"
 
@@ -28,7 +30,8 @@ class ZhviNeighborhoodDataGateway():
             state: str = "",
             city: str = "",
             metro: str = "",
-            county_name: str = ""
+            county_name: str = "",
+            zhvi_history: List[ZhviHistoryItem] = []
     ):
         if record is None:
             record = ZhviNeighborhoodRecord(
@@ -41,5 +44,6 @@ class ZhviNeighborhoodDataGateway():
                 city=city,
                 metro=metro,
                 county_name=county_name,
+                zhvi_history=zhvi_history
             )
         self.collection.insert_one(record.to_doc())
