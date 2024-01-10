41b0f7da0a12c7d7447f6755ce6079fc49ba313d
sampool <sambpool@gmail.com>
2024-01-10 07:31:06 -0700

    add get_region_records

--- neighbor_price/region_detailer.py
+++ neighbor_price/region_detailer.py
@@ -10,56 +10,66 @@ from neighbor_price.region_links import StateLink, MetroLink, CityLink, Neighbor
 class RegionDetailer:
     data_gateway: RegionDataGateway
 
+    def get_region_records(
+            self,
+            state_id: str | None = None,
+            metro_id: str | None = None,
+            city_id: str | None = None,
+            neighborhood_id: str | None = None
+    ) -> RegionRecords:
+
+        u_r = self.data_gateway.get_us_record()
+        s_r = self.data_gateway.get_region_by_id(region_id=state_id) if state_id is not None else None
+        m_r = self.data_gateway.get_region_by_id(region_id=metro_id) if metro_id is not None else None
+        c_r = self.data_gateway.get_region_by_id(region_id=city_id) if city_id is not None else None
+        n_r = self.data_gateway.get_region_by_id(region_id=neighborhood_id) if neighborhood_id is not None else None
+
+        return RegionRecords(
+            us=u_r,
+            state=s_r,
+            metro=m_r,
+            city=c_r,
+            neighborhood=n_r
+        )
+
     def get_us_detail(self) -> RegionDetail:
-        us_record = self.data_gateway.get_us_record()
+        region_records = self.get_region_records()
         state_records = self.data_gateway.get_all_states()
         return RegionDetail(
-            region_records=RegionRecords(
-                us=us_record
-            ),
+            region_records=region_records,
             links=list(map(lambda record: StateLink(
                 region_id=record.region_id,
                 label=record.region_name,
             ), state_records)),
-            prices=RegionPrices(us=us_record.region_history.get_prices()),
-            dates=us_record.region_history.get_dates()
+            prices=RegionPrices(us=region_records.us.region_history.get_prices()),
+            dates=region_records.us.region_history.get_dates()
         )
 
     def get_state_detail(self, state_id) -> RegionDetail:
-        us_record = self.data_gateway.get_us_record()
-        state_record = self.data_gateway.get_region_by_id(region_id=state_id)
-        metro_records = self.data_gateway.get_all_metros_for_state(state_name=state_record.region_name)
+        region_records = self.get_region_records(state_id=state_id)
+        metro_records = self.data_gateway.get_all_metros_for_state(state_name=region_records.state.region_name)
         return RegionDetail(
-            region_records=RegionRecords(
-                us=us_record,
-                state=state_record
-            ),
+            region_records=region_records,
             links=list(map(lambda record: MetroLink(
                 label=record.region_name,
                 region_id=record.region_id,
                 state_id=state_id
             ), metro_records)),
             prices=RegionPrices(
-                us=us_record.region_history.get_prices(),
-                state=state_record.region_history.get_prices()
+                us=region_records.us.region_history.get_prices(),
+                state=region_records.state.region_history.get_prices()
             ),
-            dates=us_record.region_history.get_dates()
+            dates=region_records.us.region_history.get_dates()
         )
 
     def get_metro_detail(self, state_id: str, metro_id: str) -> RegionDetail:
-        us_record = self.data_gateway.get_us_record()
-        state_record = self.data_gateway.get_region_by_id(region_id=state_id)
-        metro_record = self.data_gateway.get_region_by_id(region_id=metro_id)
+        region_records = self.get_region_records(state_id=state_id, metro_id=metro_id)
         city_records = self.data_gateway.get_all_cities_for_metro(
-            metro_name=metro_record.region_name,
-            state_abbrev=metro_record.state_name
+            metro_name=region_records.metro.region_name,
+            state_abbrev=region_records.metro.state_name
         )
         return RegionDetail(
-            region_records=RegionRecords(
-                us=us_record,
-                state=state_record,
-                metro=metro_record,
-            ),
+            region_records=region_records,
             links=list(map(lambda record: CityLink(
                 label=record.region_name,
                 region_id=record.region_id,
@@ -67,11 +77,11 @@ class RegionDetailer:
                 metro_id=metro_id
             ), city_records)),
             prices=RegionPrices(
-                us=us_record.region_history.get_prices(),
-                state=state_record.region_history.get_prices(),
-                metro=metro_record.region_history.get_prices()
+                us=region_records.us.region_history.get_prices(),
+                state=region_records.state.region_history.get_prices(),
+                metro=region_records.metro.region_history.get_prices()
             ),
-            dates=us_record.region_history.get_dates()
+            dates=region_records.us.region_history.get_dates()
         )
 
     def get_city_detail(
@@ -80,21 +90,17 @@ class RegionDetailer:
             metro_id: str,
             city_id: str
     ) -> RegionDetail:
-        us_record = self.data_gateway.get_us_record()
-        state_record = self.data_gateway.get_region_by_id(region_id=state_id)
-        metro_record = self.data_gateway.get_region_by_id(region_id=metro_id)
-        city_record = self.data_gateway.get_region_by_id(region_id=city_id)
+        region_records = self.get_region_records(
+            state_id=state_id,
+            metro_id=metro_id,
+            city_id=city_id
+        )
         neighborhood_records = self.data_gateway.get_all_neighborhoods_for_city(
-            state_abbrev=metro_record.state_name,
-            city_name=city_record.region_name
+            state_abbrev=region_records.metro.state_name,
+            city_name=region_records.city.region_name
         )
         return RegionDetail(
-            region_records=RegionRecords(
-                us=us_record,
-                state=state_record,
-                metro=metro_record,
-                city=city_record
-            ),
+            region_records=region_records,
             links=list(map(lambda record: NeighborhoodLink(
                 label=record.region_name,
                 region_id=record.region_id,
@@ -103,12 +109,12 @@ class RegionDetailer:
                 city_id=city_id
             ), neighborhood_records)),
             prices=RegionPrices(
-                us=us_record.region_history.get_prices(),
-                state=state_record.region_history.get_prices(),
-                metro=metro_record.region_history.get_prices(),
-                city=city_record.region_history.get_prices()
+                us=region_records.us.region_history.get_prices(),
+                state=region_records.state.region_history.get_prices(),
+                metro=region_records.metro.region_history.get_prices(),
+                city=region_records.city.region_history.get_prices()
             ),
-            dates=us_record.region_history.get_dates()
+            dates=region_records.us.region_history.get_dates()
         )
 
     def get_neighborhood_detail(
@@ -118,26 +124,21 @@ class RegionDetailer:
             city_id: str,
             neighborhood_id: str
     ) -> RegionDetail:
-        us_record = self.data_gateway.get_us_record()
-        state_record = self.data_gateway.get_region_by_id(region_id=state_id)
-        metro_record = self.data_gateway.get_region_by_id(region_id=metro_id)
-        city_record = self.data_gateway.get_region_by_id(region_id=city_id)
-        neighborhood_record = self.data_gateway.get_region_by_id(region_id=neighborhood_id)
+        region_records = self.get_region_records(
+            state_id=state_id,
+            metro_id=metro_id,
+            city_id=city_id,
+            neighborhood_id=neighborhood_id
+        )
         return RegionDetail(
-            region_records=RegionRecords(
-                us=us_record,
-                state=state_record,
-                metro=metro_record,
-                city=city_record,
-                neighborhood=neighborhood_record
-            ),
+            region_records=region_records,
             links=[],
             prices=RegionPrices(
-                us=us_record.region_history.get_prices(),
-                state=state_record.region_history.get_prices(),
-                metro=metro_record.region_history.get_prices(),
-                city=city_record.region_history.get_prices(),
-                neighborhood=neighborhood_record.region_history.get_prices()
+                us=region_records.us.region_history.get_prices(),
+                state=region_records.state.region_history.get_prices(),
+                metro=region_records.metro.region_history.get_prices(),
+                city=region_records.city.region_history.get_prices(),
+                neighborhood=region_records.neighborhood.region_history.get_prices()
             ),
-            dates=us_record.region_history.get_dates()
+            dates=region_records.us.region_history.get_dates()
         )
