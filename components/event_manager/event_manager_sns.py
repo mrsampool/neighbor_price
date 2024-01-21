acd741b7c9a5fce972e13630401ba7d25e92ae71
sampool <sambpool@gmail.com>
2024-01-20 17:04:41 -0700

    implement event_manager_sns

--- components/event_manager/event_manager_sns.py
+++ components/event_manager/event_manager_sns.py
@@ -1,4 +1,7 @@
+import logging
 from dataclasses import dataclass
+import boto3
+
 from components.event_manager.event_manager import EventManager
 from components.event_manager.event_body import EventBody
 
@@ -6,8 +9,16 @@ from components.event_manager.event_body import EventBody
 @dataclass
 class EventManagerSNS(EventManager):
 
+    def __init__(self, region_name, topic_arn):
+        self.sns_client = boto3.client('sns', region_name=region_name)
+        self.topic_arn = topic_arn
+
     def publish(self, body: EventBody):
-        raise NotImplementedError
+        response = self.sns_client.publish(
+            TopicArn=self.topic_arn,
+            Message=body.data
+        )
+        logging.info(f"publish response: {response}")
 
     def consume(self, callback):
         raise NotImplementedError
